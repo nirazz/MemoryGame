d5facd28a0a9cac38dbf992ab999a9f309232e62
nirazz <58972577+nirazz@users.noreply.github.com>
2020-03-03 11:35:27 +0200

    upload basic memory game

---
+++ Weekly Assignment - Memory Game/Script.js
@@ -0,0 +1,77 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let flippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!flippedCard) {
+    flippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  [flippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+if (isMatch.length === 12) {window.alert = "You Won!"};
+
+
+
+
+// function closeModal(){
+//   closeicon.addEventListener("click", function(e){
+//       modal.classList.remove("show");
+//       startGame();
+//   });
+// }
+
+// function playAgain(){
+//   modal.classList.remove("show");
+//   startGame();
+// }
